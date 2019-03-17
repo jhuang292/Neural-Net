ee06de2a98755861ab361e51cfa8ceb4926b4900
jhuang292 <jhuang292@wisc.edu>
2019-03-16 23:42:18 -0500

    logistic

--- logistic.py
+++ logistic.py
@@ -3,9 +3,16 @@ import sys
 import numpy as np
 
 def preprocess_json(json_file_name):
-    json_file_data = open(json_file_name, 'r')
-    print(len(json_file_data))
-        
+    
+    with open(json_file_name, 'r') as f:
+        json_dict = json.load(f)
+    json_data = json_dict['data']
+    json_features = json_dict['metadata']['features']
+    for item in json_features:
+        if item[1] != 'numeric':
+            print(1)
+        else:
+            print(0)
 
 if __name__ == '__main__':
 
